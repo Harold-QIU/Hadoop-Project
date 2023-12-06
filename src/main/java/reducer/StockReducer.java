f05f015490e4b97adf509fc5bd49a7ff36f0a251
Harold Churchill <qypyth6808@outlook.com>
2023-12-06 12:01:11 +0800

    Add datatime conversion to reducer Add some javadoc for reducer

--- src/main/java/reducer/StockReducer.java
+++ src/main/java/reducer/StockReducer.java
@@ -4,13 +4,27 @@ import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
 
 import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.HashSet;
 
 public class StockReducer extends Reducer<Text, Text, Text, Text> {
     protected String[] order;
     protected String[] trade;
     protected String v;
+    /**
+     * 用于存储MarketOrder的价格
+     */
     protected HashSet<String> priceSet = new HashSet<>();
+    /**
+     * 原时间格式
+     */
+    SimpleDateFormat inputFormat = new SimpleDateFormat("yyyyMMddHHmmssSSS");
+    /**
+     * 转换后的时间格式
+     */
+    SimpleDateFormat outputFormat = new SimpleDateFormat("M/d/yyyy h:mm:ss a");
     @Override
     protected void reduce(Text key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
         // 判断传入value是order还是trade
@@ -34,13 +48,13 @@ public class StockReducer extends Reducer<Text, Text, Text, Text> {
 
         // 处理SpecOrder
         if (order[4].equals("U")) {
-            v = order[2] + ",," + order[1] + "," + order[3] + "," + order[4] + ",," + 2;
+            v = tConvert(order[2]) + ",," + order[1] + "," + order[3] + "," + order[4] + ",," + 2;
             context.write(null, new Text(v));
         }
 
         // 处理LimitedOrder
         if (order[4].equals("2")) {
-            v = order[2] + "," + order[0] + "," + order[1] + "," + order[3] + "," + order[4] + ",," + 2;
+            v = tConvert(order[2]) + "," + order[0] + "," + order[1] + "," + order[3] + "," + order[4] + ",," + 2;
             context.write(null, new Text(v));
         }
 
@@ -51,15 +65,29 @@ public class StockReducer extends Reducer<Text, Text, Text, Text> {
 
         // 处理Cancel
         if (trade[2].equals("4")) {
-            v = trade[3] + ",," + trade[1] + "," +  order[3] + "," + order[4] + ",," + 1;
+            v = tConvert(trade[3]) + ",," + trade[1] + "," +  order[3] + "," + order[4] + ",," + 1;
             context.write(null, new Text(v));
         }
 
         // 处理MarketOrder
         if (trade[2].equals("F")) {
-            v = order[2] + ",," + order[1] + "," + order[3] + "," + order[4] + "," + priceSet.size() + "," + 2;
+            v = tConvert(order[2]) + ",," + order[1] + "," + order[3] + "," + order[4] + "," + priceSet.size() + "," + 2;
             context.write(null, new Text(v));
         }
 
+    }
+
+    /**
+     * @param t 转换前的时间
+     * @return 转换后的时间
+     */
+    protected String tConvert(String t) {
+        Date date = null;
+        try {
+            date = inputFormat.parse(t);
+        } catch (ParseException e) {
+            throw new RuntimeException(e);
+        }
+        return outputFormat.format(date);
     }
 }
