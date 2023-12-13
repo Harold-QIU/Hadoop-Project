5dd301995268b84ef85cd492179bdc72e509e61a
Harold Churchill <qypyth6808@outlook.com>
2023-12-13 16:00:47 +0800

    feat:
    Add manual sort to StockAnalysis

--- src/main/java/driver/StockAnalysis.java
+++ src/main/java/driver/StockAnalysis.java
@@ -13,6 +13,9 @@ import reducer.StockReducer;
 
 
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
 
 
 public class StockAnalysis {
@@ -37,6 +40,23 @@ public class StockAnalysis {
 
         // 提交任务并等待完成
         System.exit(job.waitForCompletion(true) ? 0 : 1);
+
+        // 排序准备：读入文件
+        List<String> lines = Files.readAllLines(Paths.get("output/part-r-00000"));
+
+        // 重写排序规则：首先按照时间戳排序，然后按照订单号排序
+        lines.sort((o1, o2) -> {
+            String[] o1s = o1.split(",");
+            String[] o2s = o2.split(",");
+            if (!o1s[0].equals(o2s[0])) {
+                return o1s[0].compareTo(o2s[0]);
+            } else {
+                return Integer.parseInt(o1s[5]) - Integer.parseInt(o2s[5]);
+            }
+        });
+
+        // 输出到文件
+        Files.write(Paths.get("output/sorted.csv"), lines);
     }
 
 }
